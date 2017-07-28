921c59365caea53d10bb1749b49611d57060a343
Nikolay Yakimov <root@livid.pp.ru>
2017-07-28 08:02:42 +0300

    [Client] Use same host

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,15 +1,13 @@
-const host = 'http://localhost:8081'
-
 import {asyncRequest} from './request'
 
 export async function render (name: string) {
-  return asyncRequest<string[]>('GET', `${host}/projects/${name}/render`)
+  return asyncRequest<string[]>('GET', `/projects/${name}/render`)
 }
 
 export async function getSource (name: string, chunk: number) {
-  return asyncRequest<string>('GET', `${host}/projects/${name}/source/${chunk}`)
+  return asyncRequest<string>('GET', `/projects/${name}/source/${chunk}`)
 }
 
 export async function update (name: string, chunk: number, value: string) {
-  return asyncRequest<string[]>('PATCH', `${host}/projects/${name}/update/${chunk}`, value)
+  return asyncRequest<string[]>('PATCH', `/projects/${name}/update/${chunk}`, value)
 }
