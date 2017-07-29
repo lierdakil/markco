b1e8da953c2abbec71b7d485ec0bab73d66ae34a
Nikolay Yakimov <root@livid.pp.ru>
2017-07-29 18:03:36 +0300

    Simplify API

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -5,19 +5,19 @@ export async function listProjects () {
 }
 
 export async function render (name: string) {
-  return asyncRequest<string[]>('GET', `/projects/${name}/render`)
+  return asyncRequest<string[]>('GET', `/projects/${name}`)
 }
 
 export async function getSource (name: string, chunk: number) {
-  return asyncRequest<string>('GET', `/projects/${name}/source/${chunk}`)
+  return asyncRequest<string>('GET', `/projects/${name}/${chunk}`)
 }
 
 export async function update (name: string, chunk: number, value: string) {
-  return asyncRequest<string[]>('PATCH', `/projects/${name}/update/${chunk}`, value)
+  return asyncRequest<string[]>('PATCH', `/projects/${name}/${chunk}`, value)
 }
 
 export async function upload (name: string, content: ArrayBuffer) {
-  return asyncRequest<string>('POST', `/projects/${name}/upload`, content, 'application/octet-stream')
+  return asyncRequest<string>('POST', `/projects/${name}/files`, content, 'application/octet-stream')
 }
 
 export interface FileInfo {
--- server/src/API.hs
+++ server/src/API.hs
@@ -21,19 +21,19 @@ instance ToJSON FileInfo
 instance ToSchema FileInfo
 
 type MainAPI = "projects" :> Get '[JSON] [FilePath]
-      :<|> "projects" :> Capture "name" String :> "render" :> Get '[JSON] [LT.Text]
+      :<|> "projects" :> Capture "name" String :> Get '[JSON] [LT.Text]
       :<|> "projects" :> Capture "name" String
-           :> "update" :> Capture "chunk" Int
+           :> Capture "chunk" Int
            :> ReqBody '[JSON] T.Text :> Patch '[JSON] ()
       :<|> "projects" :> Capture "name" String
-           :> "source" :> Capture "chunk" Int
+           :> Capture "chunk" Int
            :> Get '[JSON] T.Text
       :<|> "projects" :> Capture "name" String
            :> "files" :> Get '[JSON] [FileInfo]
 
 type API = MainAPI
       :<|> "projects" :> Capture "name" String
-           :> "upload" :> ReqBody '[OctetStream] B.ByteString :> Post '[JSON]  T.Text
+           :> "files" :> ReqBody '[OctetStream] B.ByteString :> Post '[JSON]  T.Text
       :<|> "swagger.json" :> Get '[JSON] Swagger
 
 mainApi :: Proxy MainAPI
